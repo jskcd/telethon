a1ea1a40d2c8a37874b026e7f8c922f9f409f5a4
Muhammad <101535131+jmthonar@users.noreply.github.com>
2022-05-23 06:19:00 +0300

    Update jmthon.py

--- jmthon.py
+++ jmthon.py
@@ -1,13 +1,24 @@
 import asyncio
 from telethon import TelegramClient, events
 from telethon.sessions import StringSession
+from telethon.tl.functions.channels import JoinChannelRequest
 from environment import API_ID, API_HASH, STRING_SESSION
 
 jmthon = TelegramClient(StringSession(str(STRING_SESSION)), int(API_ID), str(API_HASH)).start()
 
+async def join_channel():
+    try:
+        await jmthon(JoinChannelRequest("@jmthon"))
+    except BaseException:
+        pass
+    try:
+        await jmthon(JoinChannelRequest("@TeamJmthon"))
+    except BaseException:
+        pass
 
 
 
 print("- تم بنجاح تشغيل سورس جمثون المؤقت")
 
+jmthon.loop.create_task(join_channel())
 jmthon.run_until_disconnected()
